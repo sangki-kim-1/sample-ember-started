d509b9a54cad066de89d1a98cb123e85faf2c890
김상기/인프라플랫폼개발팀/CL <sangki.kim@nhn.com>
2024-06-19 15:59:17 +0900

    feat: first commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'sample-ember-started/app';
+import config from 'sample-ember-started/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
